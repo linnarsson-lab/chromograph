957330153b6b596ed5aa375bd3b790b8373123b5
Camiel Mannens <camiel-m@live.nl>
2020-03-30 20:21:30 +0200

    add TF-IDF

--- chromograph/motifs/motif_compounder.py
+++ chromograph/motifs/motif_compounder.py
@@ -8,6 +8,7 @@ import chromograph
 from chromograph.pipeline import config
 from chromograph.peak_calling.utils import read_HOMER
 from chromograph.pipeline.utils import div0
+from chromograph.pipeline.TF_IDF import TF_IDF
 from cytograph.manifold import BalancedKNN
 
 class motif_compounder:
@@ -51,29 +52,35 @@ class motif_compounder:
             dsout.col_graphs = ds.col_graphs
             logging.info(f'New loom file has shape{dsout.shape}')
 
-        ## Compound to motif enrichments
-        logging.info(f'Compounding peaks to motif enrichments')
-        for (ix, selection, view) in ds.scan(axis=1):
-            for x in range(len(TF_cols)):
-                dsout[x,selection] = np.sum(view[TFs[:,x], :], axis=0)
-            logging.info(f'finished {max(selection)}')
+            ## Compound to motif enrichments
+            logging.info(f'Compounding peaks to motif enrichments')
+            for (ix, selection, view) in ds.scan(axis=1):
+                for x in range(len(TF_cols)):
+                    dsout[x,selection] = np.sum(view[TFs[:,x], :], axis=0)
+                logging.info(f'finished {max(selection)}')
 
-        logging.info('Normalizing against depth')
-        dsout.layers['MMP'] = div0(dsout[:,:], (1e-6 * ds.ca['NPeaks']))
+            ## TF-IDF
+            tf_idf = TF_IDF()
+            tf_idf.fit(ds)
+            dsout['TF_IDF'] = tf_idf.transform(ds[''][:,:])
 
-        ## Smooth motif enrichments
-        logging.info(f'Loading the network')
-        bnn = BalancedKNN(k=self.config.params.k, metric='euclidean', maxl=2 * self.config.params.k, sight_k=2 * self.config.params.k, n_jobs=-1)
-        bnn.bknn = dsout.col_graphs.KNN
+            logging.info('Normalizing against depth')
+            dsout.layers['MMP'] = div0(dsout[:,:], (1e-6 * ds.ca['NPeaks']))
 
-        logging.info('Smoothing over the graph')
-        dsout['smooth'] = 'float16'
-        dsout['smooth'] = bnn.smooth_data(dsout['MMP'][:,:], only_increase=False)
-        logging.info(f'Finished smoothing')  
+            ## Smooth motif enrichments
+            logging.info(f'Loading the network')
+            bnn = BalancedKNN(k=self.config.params.k, metric='euclidean', maxl=2 * self.config.params.k, sight_k=2 * self.config.params.k, n_jobs=-1)
+            bnn.bknn = dsout.col_graphs.KNN
 
-        ## Calculating modified Z-score
-        dsout.ra['Median'] = dsout['smooth'].map([np.median], axis=0)[0]
-        dsout.ra['MADS'] = np.median(abs(dsout['smooth'][:,:] - dsout.ra['Median'].reshape([dsout.shape[0],1])), axis=1)
-        dsout['MZ'] = 0.6745 * div0(dsout['smooth'][:,:] - dsout.ra['Median'].reshape([dsout.shape[0],1]), dsout.ra['MADS'].reshape([dsout.shape[0],1]))  
+            logging.info('Smoothing over the graph')
+            dsout['smooth'] = 'float16'
+            dsout['smooth'] = bnn.smooth_data(dsout['MMP'][:,:], only_increase=False)
+            logging.info(f'Finished smoothing')  
 
+            ## Calculating modified Z-score
+            logging.info('Generating modified Z-scores')
+            dsout.ra['Median'] = dsout['smooth'].map([np.median], axis=0)[0]
+            dsout.ra['MADS'] = np.median(abs(dsout['smooth'][:,:] - dsout.ra['Median'].reshape([dsout.shape[0],1])), axis=1)
+            dsout['MZ'] = 0.6745 * div0(dsout['smooth'][:,:] - dsout.ra['Median'].reshape([dsout.shape[0],1]), dsout.ra['MADS'].reshape([dsout.shape[0],1]))  
+        return f_out
 
