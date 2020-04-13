1ecefff16f1abfb9318b582ab0e229f8c192e46e
Camiel Mannens <camiel-m@live.nl>
2020-04-13 14:43:38 +0200

    update chromgen workflow

---
+++ chromograph/preprocessing/.ipynb_checkpoints/chromgen_workflow.py
@@ -0,0 +1,42 @@
+import sys
+import numpy as np
+import os
+import sys
+import pybedtools
+from pybedtools import BedTool
+import collections
+import csv
+import matplotlib.pyplot as plt
+import gzip
+import loompy
+import scipy.sparse as sparse
+import json
+import urllib.request
+
+from chromograph.preprocessing.Chromgen import *
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%H:%M:%S')
+
+bsize = 5000
+
+sample = sys.argv[1]
+if os.path.exists(f"/data/proj/chromium/{sample}_AB_1"):
+    indir = f"/data/proj/chromium/{sample}_AB_1"
+    logging.info('Using AB file')
+elif os.path.exists(f"/data/proj/chromium/{sample}_A_1"):
+    indir = f"/data/proj/chromium/{sample}_A_1"
+    logging.info('Using A file')
+elif os.path.exists(f"/data/proj/chromium/{sample}"):
+    indir = f"/data/proj/chromium/{sample}"
+    logging.info('Using	unlabelled file')
+else:
+    logging.info("Could not find sample")
+outdir = f"/data/proj/scATAC/samples/{sample}"
+
+chromgen = Chromgen()
+chromgen.fit(indir = indir, bsize = bsize, outdir = outdir)
