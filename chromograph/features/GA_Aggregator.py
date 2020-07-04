c50c145f1e62b12d3ed5c15db0579a315baecad6
Camiel Mannens <camiel-m@live.nl>
2020-07-04 17:39:54 +0200

    Update manifolds after GA

--- chromograph/features/GA_Aggregator.py
+++ chromograph/features/GA_Aggregator.py
@@ -131,13 +131,14 @@ class GA_Aggregator:
             # logging.info("Graph skeletonization")
             # GraphSkeletonizer(min_pct=1).abstract(ds, dsout)
 
-            ## Plot results 
+            ## Update Manifolds with markers
             name = out_file.split('/')[-1].split('_')[0]
-            # logging.info("Plotting UMAP")
-            # cgplot.manifold(ds, os.path.join(self.outdir, f"{name}_bins_manifold_UMAP.png"), list(dsout.ca.Most_enriched), embedding = 'UMAP')
-            # logging.info("Plotting TSNE")
-            # cgplot.manifold(ds, os.path.join(self.outdir, f"{name}_bins_manifold_TSNE.png"), list(dsout.ca.Most_enriched), embedding = 'TSNE')
+            logging.info("Plotting UMAP")
+            cgplot.manifold(ds, os.path.join(self.outdir, f"{name}_bins_manifold_UMAP.png"), list(dsout.ca.Most_enriched), embedding = 'UMAP')
+            logging.info("Plotting TSNE")
+            cgplot.manifold(ds, os.path.join(self.outdir, f"{name}_bins_manifold_TSNE.png"), list(dsout.ca.Most_enriched), embedding = 'TSNE')
 
+            ## Other gene related plots
             cgplot.TF_heatmap(ds, dsout, os.path.join(self.outdir, f"{name}_TFs_heatmap.pdf"), layer="")
             cgplot.TF_heatmap(ds, dsout, os.path.join(self.outdir, f"{name}_TFs_heatmap_smoothed.pdf"), layer="smooth")
             cgplot.markerheatmap(ds, dsout, os.path.join(self.outdir, f"{name}_markers_heatmap.pdf"), layer="")
